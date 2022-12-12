e08e5e89e662ccd5099b8ea391f1b4a61d6e681a
Aleksey Levada <aleksey.levada@gmail.com>
2022-12-12 15:22:01 +0300

    'Update-live_search-in-goods'

--- src/pages/goods/index.js
+++ src/pages/goods/index.js
@@ -7,7 +7,7 @@ import GoodItem from '../../components/good_item/index.js'
  * Класс для генерирования страницы приложения 'Товары'
  */
 
-const inputSearchRef = React.createRef()
+//const inputSearchRef = React.createRef()
 
 class Goods extends React.Component {
     constructor() {
@@ -18,40 +18,45 @@ class Goods extends React.Component {
         }
     }
 
-    // findGood(e) {
-    //     //e.preventDefault()
-    //     this.state.goods.find(element => {
-    //         if (element.TITLE.toLowerCase() === e.target.value.toLowerCase()){
-    //             this.setState({
-    //                 goods:[element]
-    //             })
-    //         }
-    //     })
-    // }
-
     findGood(e) {
-        e.preventDefault()
-        const valueFromInputSearchInput = inputSearchRef?.current?.value
-        console.log(valueFromInputSearchInput)
-        const searchElement = this.state.goods.find(element => {
-            return element.TITLE.toLocaleLowerCase() == valueFromInputSearchInput.toLowerCase() || element.DISCR.toLocaleLowerCase() == valueFromInputSearchInput.toLowerCase()
-        })
+        this.state.goods.find(element => {
+            if (e.target.value.toLowerCase() === element.TITLE.toLowerCase()) {
+                this.setState({
+                    goods: [element]
+                })
+            }
 
-        if (searchElement == '' || searchElement == undefined) {
-            this.setState({
-                goods: goodsJSON
-            })
-        } else {
-            this.setState({
-                filteredGoods: [searchElement]
-            })
-        }
+            else if (e.target.value.toLowerCase() === '' || e.target.value.toLowerCase() === null){
+                this.setState({
+                    goods: goodsJSON
+                })
+            }
+        })
     }
 
+    // findGood(e) {
+    //     e.preventDefault()
+    //     const valueFromInputSearchInput = inputSearchRef?.current?.value
+    //     console.log(valueFromInputSearchInput)
+    //     const searchElement = this.state.goods.find(element => {
+    //         return element.TITLE.toLocaleLowerCase() == valueFromInputSearchInput.toLowerCase() || element.DISCR.toLocaleLowerCase() == valueFromInputSearchInput.toLowerCase()
+    //     })
 
+    //     if (searchElement == '' || searchElement == undefined) {
+    //         this.setState({
+    //             goods: goodsJSON
+    //         })
+    //     } else {
+    //         this.setState({
+    //             filteredGoods: [searchElement]
+    //         })
+    //     }
+    // }
+
+    /**ref={inputSearchRef} */
 
     render() {
-        const goods = this.state.filteredGoods || this.state.goods
+        // const goods = this.state.filteredGoods || this.state.goods
         return (
             <div className='container__goods'>
                 <h1>
@@ -59,13 +64,13 @@ class Goods extends React.Component {
                 </h1>
 
                 <form className='container__goods_form'>
-                    <input type='text' placeholder="Поиск товара" ref={inputSearchRef} />
-                    <input type='submit' value='Найти' onClick={(e) => this.findGood(e)} />
+                    <input type='text' placeholder="Поиск товара" onChange={(e) => this.findGood(e)} />
+                    {/* <input type='submit' value='Найти' onClick={(e) => this.findGood(e)} /> */}
                 </form>
 
                 <div className='container__card'>
                     {
-                        goods.map((element, index) => {
+                        this.state.goods.map((element) => {
                             return <GoodItem key={element.ID} data={element} />
                         })
                     }
