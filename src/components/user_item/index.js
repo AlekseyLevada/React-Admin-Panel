b7076e15bf9c8b228e7ca054da5b69b818645f43
Aleksey <aleksey.levada@gmail.com>
2023-01-05 11:23:22 +0300

    'Сформировал-примерную-структуру-таблицы-с-юзерами'

--- src/components/user_item/index.js
+++ src/components/user_item/index.js
@@ -1,80 +1,49 @@
 import './style.css'
+import {ReactComponent as Trashbox} from '../../../src/icons/trashbox.svg'
+import {ReactComponent as PencilEdit} from '../../../src/icons/editpencil.svg'
+
 import React from 'react'
+import {Link} from 'react-router-dom'
+
 
 export function UserItem(props) {
-
     const { data } = props
-
     return (
-        <>
-            <div className='user__item user'>
-                
-                <div>
-                    <div classname='user__table-header'>
-                        
-                    </div>
-                    <div className='user__checkbox'>
-                        <input type='checkbox'/>
-                    </div>
-                </div>
-                <div>
-                    <div classname='user__table-header'>
-                        ID
-                    </div>
-                    <div className='user__id'>
-                        {data.id}
-                    </div>
-                </div>
-                <div>
-                    <div classname='user__table-header'>
-                        Имя
-                    </div>
-                    <div className='user__name'>
-                        {data.name}
-                    </div>
-                </div>
-                <div>
-                    <div classname='user__table-header'>
-                        Аватар
-                    </div>
-                    <div className='user__avatar'>
-                        <img src={data.avatar}/>
-                    </div>
-                </div>
-                <div>
-                    <div classname='user__table-header'>
-                        Почтовый ящик
-                    </div>
-                    <div className='user__email'>
-                        {data.email}
-                    </div>
-                </div>
-                <div>
-                    <div classname='user__table-header'>
-                        Телефон
-                    </div>
-                    <div className='user__phone'>
-                        {data.phone}
-                    </div>
-                </div>
-                <div>
-                    <div classname='user__table-header'>
-                        Сайт
-                    </div>
-                    <div className='user__website'>
-                        {data.website}
-                    </div>
-                </div>
-                <div>
-                    <div classname='user__table-header'>
-                        Компания
-                    </div>
-                    <div className='user__company'>
-                        {data.company}
-                    </div>
-                </div>
-                
+        <div className='table__item item'>
+            <div className='item__table_cell'>
+                <input type='checkbox' />
             </div>
-        </>
+            <div className='item__table_cell'>
+                <div>{data.id}</div>
+            </div>
+            <div className='item__table_cell'>
+                <div>{data.name}</div>
+            </div>
+            <div className='item__table_cell'>
+                <div><img src={data.avatar} /></div>
+            </div>
+            <div className='item__table_cell'>
+                <div>{data.email}</div>
+            </div>
+            <div className='item__table_cell'>
+                <div>{data.phone}</div>
+            </div>
+            <div className='item__table_cell'>
+                <div>{data.website}</div>
+            </div>
+            <div className='item__table_cell'>
+                <div>{data.company}</div>
+            </div>
+            <div className='item__table_cell'>
+                <div className='item__table_action action'>
+                    <div className='action__edit'>
+                        <PencilEdit className='pencil__edit'/>
+                    </div>
+                    <div className='action__delete'>
+                        <Trashbox className='trashbox'/>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
--- src/components/user_list/index.js
+++ src/components/user_list/index.js
@@ -1,11 +1,11 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import './style.css'
 
-import {UserItem} from '../user_item/index.js'
+import { UserItem } from '../user_item/index.js'
 import usersJSON from '../../stub/users.json'
 
 export function UserList() {
-    
+
     const [users, setUsers] = useState(usersJSON)
 
     return (
@@ -13,18 +13,47 @@ export function UserList() {
             <h2>
                 Пользователи
             </h2>
-            <form>
-                <input type='text' placeholder='Данные пользователя'/>
+            <form className='container__users_form'>
+                <input type='text' placeholder='Данные пользователя' />
                 <button>Удалить ... пользователей</button>
                 <button>Добавить пользователя</button>
             </form>
-            <ul className='users__table'>
+            <div className='users__table table'>
+                <div className='table__header'>
+                    <div className='table__cell'>
+                        <div>Выбор</div>
+                    </div>
+                    <div className='table__cell'>
+                        <div>ID</div>
+                    </div>
+                    <div className='table__cell'>
+                        <div>Имя</div>
+                    </div>
+                    <div className='table__cell'>
+                        <div>Аватар</div>
+                    </div>
+                    <div className='table__cell'>
+                        <div>Почта</div>
+                    </div>
+                    <div className='table__cell'>
+                        <div>Телефон</div>
+                    </div>
+                    <div className='table__cell'>
+                        <div>Вебсайт</div>
+                    </div>
+                    <div className='table__cell'>
+                        <div>Компания</div>
+                    </div>
+                    <div className='table__cell'>
+                        <div>Действие</div>
+                    </div>
+                </div>
                 {
                     users.map(el => {
-                        return <UserItem key={el.id} data={el}/>
+                        return <UserItem key={el.id} data={el} />
                     })
                 }
-            </ul>
+            </div>
         </div>
     )
 }
