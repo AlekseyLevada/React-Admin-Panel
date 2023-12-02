a95bd15498cabec19a07c2bb137d23ad8ca5212d
Aleksey Levada <aleksey.levada@gmail.com>
2023-12-02 09:15:41 +0300

    Правки кода

--- src/components/menu/index.js
+++ src/components/menu/index.js
@@ -1,28 +1,30 @@
-import './style.css'
-import React from 'react'
-import PropTypes from 'prop-types'
-import { Link } from 'react-router-dom'
+import "./style.css"
+import React from "react"
+import PropTypes from "prop-types"
+import { Link } from "react-router-dom"
 
-import { MenuItemShape } from '../../shapes/MenuItemShape'
+import { MenuItemShape } from "../../shapes/MenuItemShape"
 
 /**
- * Компонент для генерирования компонента 'Menu' приложения 
+ * Компонент для генерирования компонента "Menu" приложения 
  */
 
 export function Menu(props) {
     const { menu } = props
 
     return (
-        <div className='container__menu menu'>
-            <h2>
-                Меню
-            </h2>
-            <ul className='menu__list'>
+        <div className="container__menu menu">
+            <div className="container__menu-title">
+                <h2>
+                    Меню
+                </h2>
+            </div>
+            <ul className="menu__list">
                 {
                     menu.map((menuElement, index) => {
                         return (
-                            <li className='menu__item' key={index}>
-                                <Link className='menu__link' to={menuElement.link}>
+                            <li className="menu__item" key={index}>
+                                <Link className="menu__link" to={menuElement.link}>
                                     {menuElement.text}
                                 </Link>
                             </li>
